23c32af7f9f17feff30fe6e62562afc048c2301e
vy211 <vy80091@gmail.com>
2024-03-05 16:05:02 +0530

    Order Service

--- Store3x.Gateways.GatewayAPI/server.js
+++ Store3x.Gateways.GatewayAPI/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require('cors');
 const proxy = require('express-http-proxy');
 
+//const express = require('express');
+console.log("hellowoeld");
 const app = express();
 
 app.use(bodyParser.json());
---
+++ Store3x.Services.OrderAPI/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store3x.Services.OrderAPI.Models
+{
+    [Table("Store3x_order")]
+    public class Order
+    {
+        [Key]
+        public int order_id { get; set; }
+        [Required]
+        public string buyer_id { get; set; }
+        public int card_id { get; set; }
+        public decimal total_price { get; set; }
+        public DateTime order_date { get; set; }
+        public decimal tax { get; set; }
+        public decimal shipping_price { get; set; }
+        public int delivery_address_id { get; set; }
+        public DateTime delivery_date { get; set; }
+        public char order_status { get; set; }
+        public int quantity { get; set; }
+
+    }
+}
