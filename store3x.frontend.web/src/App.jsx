e51e91beed978296953f094478b89924c9e6cd81
Sandeep Kumar Singh <sandeepkumar.k@simplify3x.com>
2024-03-06 13:45:38 +0530

    dsfh

--- store3x.frontend.web/src/App.jsx
+++ store3x.frontend.web/src/App.jsx
@@ -1,30 +1,34 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import LoginPage from './components/Login/LoginPage';
-import SignUp from './components/Login/SignUp';
-import ForgotPassword from './components/Login/ForogotPass';
-import Home from './components/Home/Home';
-import Product from './components/Product/Product';
-import Header from './components/Header/Header';
 
 function App() {
-    
+  const [count, setCount] = useState(0)
 
   return (
-      <Router>
-          <div>
-             
-              <Routes>
-                  <Route path="/" element={<Home/> } />
-                  <Route path="/login" element={<LoginPage />} />
-                  <Route path="/signup" element={<SignUp />} />
-                  <Route path="/product" element={<Product />} />
-                  <Route path="/forgot-password" element={<ForgotPassword />} />
-                  
-              </Routes>
-          </div>
-      </Router>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
