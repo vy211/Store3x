95c229c82647013c4ae9e4a2790ea00c54835731
Sandeep Kumar Singh <sandeepkumar.k@simplify3x.com>
2024-03-14 16:50:49 +0530

    cors added in AuthApi

--- Store3x.Services.AuthAPI/server.js
+++ Store3x.Services.AuthAPI/server.js
@@ -45,7 +45,7 @@ poolConnect.then(() => {
     app.use((req, res, next) => {
         console.log("HTTP Method : " + req.method + " , URL : " + req.url);
         next();
-    })
+    });
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
