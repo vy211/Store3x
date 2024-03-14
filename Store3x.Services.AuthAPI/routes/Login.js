6cae71b91340a57d72955d20b72f9e5a75f9f09f
Sandeep Kumar Singh <sandeepkumar.k@simplify3x.com>
2024-03-14 16:50:49 +0530

    cors added in AuthApi

--- Store3x.Services.AuthAPI/routes/Login.js
+++ Store3x.Services.AuthAPI/routes/Login.js
@@ -4,19 +4,28 @@ const router = express.Router();
 const sql = require("mssql/msnodesqlv8");
 const bcrypt = require("bcrypt");
 
+// Function to fetch user data from the database
+async function fetchUserFromDB(pool, email) {
+    try {
+        const request = new sql.Request(pool);
+        const result = await request.query(`SELECT * FROM store3x_user WHERE email='${email}'`);
+        return result.recordset.length > 0 ? result.recordset[0] : null;
+    } catch (error) {
+        console.error("Error fetching user data:", error.message);
+        throw error; // Propagate the error
+    }
+}
+
 router.post("/", async (req, res) => {
     try {
         const { pool } = req.app.locals;
         const { email, password } = req.body;
-        const request = new sql.Request(pool);
 
-        const result = await request.query(`
-            SELECT * FROM store3x_user WHERE email='${email}'
-        `);
+        // Fetch user from the database
+        const user = await fetchUserFromDB(pool, email);
+        console.log(user);
 
-        if (result.recordset.length > 0) {
-            const user = result.recordset[0];
-
+        if (user) {
             // Compare the hashed password from the database with the password provided by the user
             const passwordMatch = await bcrypt.compare(password, user.password);
 
@@ -33,20 +42,21 @@ router.post("/", async (req, res) => {
                 res.cookie('jwt', token, { httpOnly: true, maxAge: 86400000 });
 
                 // Respond with a success status code (200) and the user information
-                return res.status(200).json({ message: "User fetched successfully", data: user, token });
+                return res.status(200).json({ message: "User fetched successfully", user, token });
             } else {
                 // Respond with an unauthorized status code (401) for incorrect password
                 return res.status(401).json({ message: "Incorrect Password !!!" });
             }
         } else {
             // Respond with a not found status code (404) for user not found
-            return res.status(404).json({ message: `Incorrect Email Id !!!` });
+            return res.status(404).json({ message: `User with email '${email}' not found` });
         }
     } catch (error) {
-        console.error("Error fetching data:", error.message);
+        console.error("Error handling request:", error.message);
         // Respond with an internal server error status code (500)
         return res.status(500).send("Internal Server Error");
     }
 });
+
 
 module.exports = router;
