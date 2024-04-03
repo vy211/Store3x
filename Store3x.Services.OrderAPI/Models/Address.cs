bdaef6c64f6a9316ae9b30b9aba3c5d7fba15ad6
vy211 <vy80091@gmail.com>
2024-04-03 15:11:49 +0530

    address

---
+++ Store3x.Services.OrderAPI/Models/Address.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store3x.Services.OrderAPI.Models
+{
+    [Table("contact_detail")]
+    public class Address
+    {
+        [Key]
+        public int address_id { get; set; }
+        [Required]
+        public string user_id { get; set; }
+        public string street1 { get; set; }
+        public string street2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public int zipcode { get; set; }
+        public string phone { get; set; }
+        public int is_default { get; set; }
+    }
+}
--- Store3x.Services.OrderAPI/Program.cs
+++ Store3x.Services.OrderAPI/Program.cs
@@ -21,6 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAll");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
